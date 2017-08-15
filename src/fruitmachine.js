5b1add4c3ebd2c8e5a8f36eb01d23e70a3ef6c06
jason chen <cjx89807@alibaba-inc.com>
2017-08-15 22:19:49 +0800

    feat: add framework

--- src/fruitmachine.js
+++ src/fruitmachine.js
@@ -11,11 +11,42 @@ function FruitMachine(options) {
   this.doc = new JC.Container();
   this.fruits = options.fruits;
   this.cursor = 0;
+  this.round = 0;
   this.direction = 1;
+
+  const This = this;
+  this.doc.on('pretimeline', function(snippet) {
+    This.update(snippet);
+  });
 }
 
 FruitMachine.prototype.setFruits = function() {
 
+};
+
+FruitMachine.prototype.update = function(snippet) {
+  if (this.tt >= this.space) {
+    this.tt = JC.Utils.euclideanModulo(this.tt, this.space);
+    this.cursor = this.cursor + this.direction;
+    if (this.spill()) {
+      this.round++;
+      this.cursor = JC.Utils.euclideanModulo(this.cursor, total);
+      if (this.round > 2) console.log('object');
+    }
+    const total = this.doc.childs.length;
+  }
+};
+
+FruitMachine.prototype.spill = function() {
+  const total = this.doc.childs.length;
+  const bs = this.cursor <= 0 && this.direction === -1;
+  const ts = this.cursor >= total && this.direction === 1;
+  return bs || ts;
+};
+
+FruitMachine.prototype.lightOne = function(idx) {
+  const fruit = this.doc.childs[idx];
+  if (fruit) fruit.lightoff();
 };
 
 FruitMachine.prototype.readGo = function() {
