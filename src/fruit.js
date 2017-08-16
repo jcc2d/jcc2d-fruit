f2cc8c5e666425abc0f246b36126b377863256c4
jason chen <cjx89807@alibaba-inc.com>
2017-08-16 22:31:59 +0800

    feat: add start up

--- src/fruit.js
+++ src/fruit.js
@@ -9,8 +9,14 @@ import {JC} from './jc';
  */
 function Fruit(options) {
   this.doc = new JC.Container();
-  this.fruit = new JC.Sprite({});
-  this.mask = new JC.Sprite({});
+  this.fruit = new JC.Sprite(options.fruit);
+  this.doc.x = options.fruit.x;
+  this.doc.y = options.fruit.y;
+
+  this.mask = new JC.Sprite(options.mask);
+  this.mask.x = options.mask.x;
+  this.mask.y = options.mask.y;
+
   this.mask.alpha = 0;
   this.doc.adds(this.fruit, this.mask);
 }
@@ -19,16 +25,17 @@ Fruit.prototype.light = function() {
   this.mask.alpha = 1;
 };
 
-Fruit.prototype.lightoff = function() {
+Fruit.prototype.lightoff = function(duration) {
   if (this.offing) return;
   const This = this;
   this.offing = true;
   this.mask.animate({
     from: {alpha: 1},
-    to: {alpha: 0},
-    duration: 400,
+    to: {alpha: 1},
+    duration,
   }, true).on('complete', function() {
     This.offing = false;
+    This.mask.alpha = 0;
   });
 };
 
--- src/fruitmachine.js
+++ src/fruitmachine.js
@@ -2,17 +2,32 @@
 /* eslint no-bitwise: 0 */
 
 import {JC} from './jc';
+import {Fruit} from './fruit';
+// import {Utils} from './utils';
 
 /**
  *
  * @param {object} options
  */
 function FruitMachine(options) {
+  this.images = options.images;
+  this.assetBox = JC.loaderUtil(options.images);
+  this.mask = options.mask;
+
   this.doc = new JC.Container();
-  this.fruits = options.fruits;
+
   this.cursor = 0;
   this.round = 0;
   this.direction = 1;
+  this.progress = 0;
+  this.step = 800;
+  this.firing = 3000;
+  this.speed = this.speedMin = 800;
+  this.speedMax = this.speed / 10;
+
+  this.fruits = [];
+
+  this.setFruits(options.fruits);
 
   const This = this;
   this.doc.on('pretimeline', function(snippet) {
@@ -20,40 +35,66 @@ function FruitMachine(options) {
   });
 }
 
-FruitMachine.prototype.setFruits = function() {
-
+FruitMachine.prototype.setFruits = function(fruits) {
+  for (let i = 0; i < fruits.length; i++) {
+    const fruit = fruits[i];
+    fruit.texture = this.assetBox.getById(fruit.ref);
+    this.mask.texture = this.assetBox.getById(this.mask.ref);
+    const mask = this.mask;
+    const fruitDoc = new Fruit({
+      fruit,
+      mask,
+    });
+    this.fruits.push(fruitDoc);
+    this.doc.adds(fruitDoc.doc);
+  }
 };
 
 FruitMachine.prototype.update = function(snippet) {
-  if (this.tt >= this.space) {
-    this.tt = JC.Utils.euclideanModulo(this.tt, this.space);
-    this.cursor = this.cursor + this.direction;
+  this.progress += snippet;
+  this.step += snippet;
+  this.startUp();
+  this.checkLight();
+};
+
+FruitMachine.prototype.startUp = function() {
+  if (this.progress <= this.firing) {
+    const rate = this.progress / this.firing;
+    this.speed = this.speedMax + (1 - rate) * (this.speedMin - this.speedMax);
+  }
+};
+
+FruitMachine.prototype.checkLight = function() {
+  if (this.step >= this.speed) {
+    this.step = JC.Utils.euclideanModulo(this.step, this.speed);
     if (this.spill()) {
+      const total = this.fruits.length;
       this.round++;
       this.cursor = JC.Utils.euclideanModulo(this.cursor, total);
-      if (this.round > 2) console.log('object');
+      // if (this.round > 2) console.log('object');
     }
-    const total = this.doc.childs.length;
+    this.lightOne();
+    this.cursor = this.cursor + this.direction;
   }
 };
 
 FruitMachine.prototype.spill = function() {
-  const total = this.doc.childs.length;
+  const total = this.fruits.length;
   const bs = this.cursor <= 0 && this.direction === -1;
   const ts = this.cursor >= total && this.direction === 1;
   return bs || ts;
 };
 
-FruitMachine.prototype.lightOne = function(idx) {
-  const fruit = this.doc.childs[idx];
-  if (fruit) fruit.lightoff();
-};
-
-FruitMachine.prototype.readGo = function() {
-
+FruitMachine.prototype.lightOne = function() {
+  const fruit = this.fruits[this.cursor];
+  if (fruit) fruit.lightoff(this.speed * 2);
 };
 
 FruitMachine.prototype.standTo = function() {
+
+};
+
+FruitMachine.prototype.cancle = function() {
 
 };
 
